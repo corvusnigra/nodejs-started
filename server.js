668d4fa9b64738b25e1626a29426b2286ffcdc2a
uriy <itemruby@gmail.com>
2016-11-17 20:56:42 +0300

    static files

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-    console.log(`Incoming url ${req.url}`);
+    
+    if(req.url.startsWith('/static/')){
+        fs.readFile(req.url.substr(1), (err, data)=>{
+            if(err){
+                console.log(`Error file not found ${err}`);
+                res.statusCode = 404;
+                res.end();
+                return
+            }
 
-    res.end('Hello world!')
+                res.end(data);
 
+        })
+        return
+    }
+    res.end('Hello World');
 }).listen(3000,'localhost',()=>{
    console.log(`Server listening on localhost port 3000`)
 });
